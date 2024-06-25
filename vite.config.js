56a3c9849f6d7d7c55a485d3008607123ee873f1
limonatasuyu <emredilek6@gmail.com>
2024-06-25 18:16:46 +0300

    changes on package.json

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb", "**/*.dae", "**/*obj"]
+  assetsInclude: ["**/*.glb", "**/*.dae", "**/*obj"],
+  base: "/vite-react-deploy/"
 })
